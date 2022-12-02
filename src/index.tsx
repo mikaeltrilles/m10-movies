18bfd1365d4b004e4c12bf9d35a9d87958a389e8
Mikael Trilles <11914687-mikaeltrilles@users.noreply.gitlab.com>
2022-12-02 12:40:45 +0100

    📦 Ajout de bootstrap

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.scss';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.js';
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
