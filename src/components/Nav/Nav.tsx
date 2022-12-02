0dd204b165bf5fc17f58be63f8065ec5e9189689
Mikael Trilles <11914687-mikaeltrilles@users.noreply.gitlab.com>
2022-12-02 12:50:12 +0100

    📦 Mise en place Nav bar

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,3 +1,27 @@
 export default function Nav() {
-  return <>NAV</>;
+  return (
+    <nav className="navbar navbar-expand-sm bg-dark navbar-dark">
+      <div className="container-fluid">
+
+        <a href="/" className="navbar-brand">Filmothèque</a>
+        <button
+          className="navbar-toggler"
+          data-bs-toggle="collapse"
+          data-bs-target="#navabar-toggler" aria-controls="navabar-toggler">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div id="main-nav" className="collapse navbar-collapse">
+          <ul className="nav">
+            <li className="nav-item">
+              <a href="/" className="nav-link active">Catalogue</a>
+            </li>
+            <li className="nav-item">
+              <a href="/" className="nav-link active">A voir</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 }
