91723ab152cf8bfb15ad684984ac7284e0296706
Michael Friedrich <mfriedrich@gitlab.com>
2022-12-06 02:27:13 +0100

    🧪 Test Router & Reducer non fonctionnel

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.js';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './router';
 import './index.module.scss';
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
---
+++ src/router.tsx
@@ -0,0 +1,47 @@
+import { createBrowserRouter, useMatch, Link, NavLink, Outlet, useRouteError } from 'react-router-dom';
+import './index.module.scss';
+
+const FourOFour = () => {
+  const error = useRouteError() as any;
+  console.log(error);
+  
+  return <>
+    <h1>{error.status} {error.statusText}</h1>
+    <Link to="/">Retour</Link>
+  </>
+};
+
+const One = () => {
+  const route = useMatch('/one');
+  console.log(route);
+  return <h1>One</h1>;
+};
+const Two = () => <h1>Two</h1>;
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <>
+      <NavLink to="/one" style={({ isActive }) => {
+        if (!isActive) return;
+        return {
+          color: 'green',
+          fontSize: '1.9rem'
+        };
+      }}>Vers One</NavLink>
+      <NavLink to="/two">Vers Two</NavLink>
+      <Outlet />
+    </>,
+    errorElement: <FourOFour />,
+    children: [
+      {
+        path: '/one',
+        element: <One />,
+      },
+      {
+        path: '/two',
+        element: <Two />
+      },
+    ]
+  },
+]);
